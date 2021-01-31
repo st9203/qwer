a8ee74f2b9d7020164333473ada6e2fb3b47fbb6
myKim <myKim@14.36.225.73>
2021-01-31 15:55:29 +0900

    가보자!

---
+++ src/main/java/com/stock/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.stock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/index", method= RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	
+}
